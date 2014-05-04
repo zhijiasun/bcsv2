5ed4d726cfe2b5a988dd5ea1f265d6fc14154ecd
zhijiasun <zhijiasun84@gmail.com>
2014-05-04 21:46:49 +0800

    modify

--- bcs/urls.py
+++ bcs/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url,patterns,include
 from bcs import views
 
 urlpatterns = patterns('',
-	url(r'^index/',views.index)
+	url(r'^index/',views.index),
+	url(r'^logout1/',views.logout1),
 )
--- bcs/views.py
+++ bcs/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render,render_to_response,redirect
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def index(request):
 	c = {}
-	return render_to_response('index.html',c)
+	print 'aa'
+	if request.user:
+		print request.user.username
+		print 'bbb'
+	else:
+		print 'no user login'
+
+	return render(request,'index.html')	
+
+def logout1(request):
+	logout(request)
+	return redirect('/bcs/index/')
+
